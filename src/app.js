f8c845668dff88bf05d5b187fc76651b003846da
LudmilaS <ludmila.https@gmail.com>
2023-02-11 14:56:20 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express, {json} from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import router from './routers/index.js'
+
+dotenv.config()
+
+const app = express()
+
+app.use(json())
+app.use(cors())
+app.use(router)
+
+app.listen(process.env.PORT)
